c79db49dd8eccc912a684a06d7227e2bea903d0c
eduardoprog <47372216+eduardoprog@users.noreply.github.com>
2019-08-14 19:21:51 -0300

    Adicionados arquivos base do site

---
+++ index.php
@@ -0,0 +1,247 @@
+<?php 
+include("cabecalho_index.php");
+include("navbar.php");
+ ?>
+	<body style="background-image: linear-gradient(black, white);">
+		<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+		  <ol class="carousel-indicators">
+		    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+		    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+		    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+		  </ol>
+		  <div class="carousel-inner">
+		    <div class="carousel-item active">
+		      <img src="imgs/img1.jpg" class="d-block w-100" alt="...">
+		    </div>
+		    <div class="carousel-item">
+		      <img src="imgs/img2.jpg" class="d-block w-100" alt="...">
+		    </div>
+		    <div class="carousel-item">
+		      <img src="imgs/img3.jpg" class="d-block w-100" alt="...">
+		    </div>
+		  </div>
+		  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+		    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+		    <span class="sr-only">Previous</span>
+		  </a>
+		  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+		    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+		    <span class="sr-only">Next</span>
+		  </a>
+		</div>
+		<div class="container-fluid">
+			<center>
+				<br>	
+				<h3>PRODUTOS EM DESTAQUE</h3>
+				<br>
+				<div class="row"><!-- INÍCIO PRIMEIRA LINHA DE PRODUTOS!-->
+					<div class="col-md-4">
+						<div class="card" style="width: 18rem;">
+						  <img src="..." class="card-img-top" alt="...">
+						  <div class="card-body">
+						    <h5 class="card-title">Card title</h5>
+						    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						    <a href="#" class="btn btn-primary">Go somewhere</a>
+						  </div>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="card" style="width: 18rem;">
+						  <img src="..." class="card-img-top" alt="...">
+						  <div class="card-body">
+						    <h5 class="card-title">Card title</h5>
+						    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						    <a href="#" class="btn btn-primary">Go somewhere</a>
+						  </div>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="card" style="width: 18rem;">
+						  <img src="..." class="card-img-top" alt="...">
+						  <div class="card-body">
+							<h5 class="card-title">Card title</h5>
+						    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						    <a href="#" class="btn btn-primary">Go somewhere</a>
+						  </div>
+						</div>
+					</div>
+				</div>
+			<br><!-- FIM PRIMEIRA LINHA DE PRODUTOS!-->
+			<!-- INÍCIO SEGUNDA LINHA DE PRODUTOS!-->
+			<div class="row">
+				<div class="col-md-4">
+					<div class="card" style="width: 18rem;">
+					  <img src="..." class="card-img-top" alt="...">
+					  <div class="card-body">
+					    <h5 class="card-title">Card title</h5>
+					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					    <a href="#" class="btn btn-primary">Go somewhere</a>
+					  </div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card" style="width: 18rem;">
+					  <img src="..." class="card-img-top" alt="...">
+					  <div class="card-body">
+					    <h5 class="card-title">Card title</h5>
+					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					    <a href="#" class="btn btn-primary">Go somewhere</a>
+					  </div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card" style="width: 18rem;">
+					  <img src="..." class="card-img-top" alt="...">
+					  <div class="card-body">
+					    <h5 class="card-title">Card title</h5>
+					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					    <a href="#" class="btn btn-primary">Go somewhere</a>
+					  </div>
+					</div>
+				</div>
+			</div><!-- FIM SEGUNDA LINHA DE PRODUTOS!-->
+			<br><!-- INÍCIO SEÇÃO DE NOVOS LANÇAMENTOS!-->
+			<div class="jumbotron" style="border-bottom-left-radius: 0px; border-bottom-right-radius: 0px; height: 300px; background-image: url(http://s4.thingpic.com/images/Xf/TrCtYh1zKfphdFXzWZZ2E8zU.jpeg);">
+				<h1 class="display-2" style="color: red;">PRÓXIMOS LANÇAMENTOS</h1>
+			</div><!-- LINHA UM!-->
+			<div class="row">
+				<div class="col-md-12">
+					<h3>2019</h3>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-3">
+					<div class="card" style="width: 18rem;">
+					  <img src="https://i.jeuxactus.com/datas/jeux/s/h/shenmue-iii/xl/shenmue-iii-jaquette-5cf530d5818ab.jpg" class="card-img-top" alt="...">
+					  <div class="card-body">
+					    <h5 class="card-title">Shenmue III</h5>
+					    <p class="card-text">YS Net, Neilo<p>
+					    <p class="card-text">Data de Lançamento - 24/08/2019</p>
+					    <a href="#" class="btn btn-primary">Go somewhere</a>
+					  </div>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="card" style="width: 18rem;">
+					  <img src="https://cdn.awsli.com.br/800x800/241/241991/produto/40395577/fc5ea0ebf4.jpg" class="card-img-top" alt="...">
+					  <div class="card-body">
+					    <h5 class="card-title">Control</h5>
+					    <p class="card-text">505 Games</p>
+					    <p class="card-text">Data de Lançamento - 27/08/2019</p>
+					    <a href="#" class="btn btn-primary">Go somewhere</a>
+					  </div>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="card" style="width: 18rem;">
+					  <img src="http://www.pressstart.pt/11044-large_default/borderlands-3-ps4.jpg" class="card-img-top" alt="...">
+					  <div class="card-body">
+					    <h5 class="card-title">Borderlands 3</h5>
+					    <p class="card-text">Gearbox Software</p>
+					    <p class="card-text">Data de Lançamento: 13/09/2019</p>
+					    <a href="#" class="btn btn-primary">Go somewhere</a>
+					  </div>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="card" style="width: 18rem;">
+					  <img src="https://store-images.s-microsoft.com/image/apps.60245.63865486515819353.ae2fad89-e111-4633-a5ef-b68dec240c64.b82fd5cd-2089-427a-9988-5507b7030619?w=800&h=800&format=jpg" class="card-img-top" alt="...">
+					  <div class="card-body">
+					    <h5 class="card-title">Star Wars: Jedi Fallen Order</h5>
+					    <p class="card-text">Electronic Arts</p>
+					    <p class="card-text">Data de Lançamento: 15/11/2019</p>
+					    <a href="#" class="btn btn-primary">Go somewhere</a>
+					  </div>
+					</div>
+				</div>
+			</div><br><!-- LINHA DOIS!-->
+			<div class="row">
+				<div class="col-md-3">
+					<div class="card" style="width: 18rem;">
+					  <img src="..." class="card-img-top" alt="...">
+					  <div class="card-body">
+					    <h5 class="card-title">Card title</h5>
+					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					    <a href="#" class="btn btn-primary">Go somewhere</a>
+					  </div>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="card" style="width: 18rem;">
+					  <img src="..." class="card-img-top" alt="...">
+					  <div class="card-body">
+					    <h5 class="card-title">Card title</h5>
+					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					    <a href="#" class="btn btn-primary">Go somewhere</a>
+					  </div>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="card" style="width: 18rem;">
+					  <img src="..." class="card-img-top" alt="...">
+					  <div class="card-body">
+					    <h5 class="card-title">Card title</h5>
+					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					    <a href="#" class="btn btn-primary">Go somewhere</a>
+					  </div>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="card" style="width: 18rem;">
+					  <img src="..." class="card-img-top" alt="...">
+					  <div class="card-body">
+					    <h5 class="card-title">Card title</h5>
+					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					    <a href="#" class="btn btn-primary">Go somewhere</a>
+					  </div>
+					</div>
+				</div>
+			</div><br><!-- LINHA TRÊS!-->
+			<div class="row">
+				<div class="col-md-3">
+					<div class="card" style="width: 18rem;">
+					  <img src="..." class="card-img-top" alt="...">
+					  <div class="card-body">
+					    <h5 class="card-title">Card title</h5>
+					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					    <a href="#" class="btn btn-primary">Go somewhere</a>
+					  </div>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="card" style="width: 18rem;">
+					  <img src="..." class="card-img-top" alt="...">
+					  <div class="card-body">
+					    <h5 class="card-title">Card title</h5>
+					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					    <a href="#" class="btn btn-primary">Go somewhere</a>
+					  </div>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="card" style="width: 18rem;">
+					  <img src="..." class="card-img-top" alt="...">
+					  <div class="card-body">
+					    <h5 class="card-title">Card title</h5>
+					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					    <a href="#" class="btn btn-primary">Go somewhere</a>
+					  </div>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<div class="card" style="width: 18rem;">
+					  <img src="..." class="card-img-top" alt="...">
+					  <div class="card-body">
+					    <h5 class="card-title">Card title</h5>
+					    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					    <a href="#" class="btn btn-primary">Go somewhere</a>
+					  </div>
+					</div>
+				</div>
+			</div>
+			</center>
+		</div>
+	</body><br>
+<?php  
+include("rodape.php");
+?>
