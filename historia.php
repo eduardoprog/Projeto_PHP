1775b53a5a3a66e37b0131314fbbd41eeb54433c
HigorMPL <54591956+HigorMPL@users.noreply.github.com>
2019-08-27 15:25:41 -0300

    Add files via upload

--- historia.php
+++ historia.php
@@ -2,6 +2,20 @@
 include("cabecalho_historia.php");
 include("navbar.php");
  ?>
+ 	<center>
+ 	<h1 style="color: white">A história da Retro Maze Game Store.</h1>
+ 	<br>
+ 	<p><h4 style="color: white">Fundado em 2003, o Retro Maze Game Store foi um dos pioneiros no comércio eletrônico brasileiro e hoje é o maior e-commerce do segmento de tecnologia na América Latina. Este ano, a empresa completa 16 anos de histórias e conquistas de um time obcecado por agilidade, qualidade de atendimento, velocidade de entrega e respeito pelo consumidor.</p>
+ 	
+	<p>Com preços imbatíveis e mais de 20 mil produtos em seu catálogo, o Retro Maze Game Store está sempre à frente e traz em primeira mão os melhores lançamentos do mercado mundial.</p>
+	
+	<p>Desde sua fundação, o Retro Maze Game Store já atendeu mais de 6 milhões de pessoas e realizou entregas em mais de 5.000 cidades. Entre as empresas clientes, destacam-se grandes grupos como Microsoft, Banco Bradesco, Banco Itaú, Petrobrás, Exxon, Walt Disney, Ford, Rede Globo, Volvo, além de vários órgãos do Governo Brasileiro.</p>
+	
+	<p>O e-commerce é um dos sites mais acessados do país e lidera o ranking das lojas virtuais mais recomendadas pelos consumidores brasileiros, no segmento de tecnologia com os principais índices de avaliação e selos de qualidade da internet.</p>
+	
+	<p>Além do e-commerce, o Retro Maze Game Store é um grande incentivador do e-sport, sendo responsável pela criação de uma das maiores equipes de League of Legends do Brasil, a Retro Maze e-Sports, tricampeã nacional e a primeira organização do país a participar de um Campeonato Mundial.</h4></p>
+	<br>
+	</center>
  	<button onclick="topFunction()" id="myBtn" title="Go to top" class="btn btn-primary">Voltar ao topo</button>
 	<body style="background-image: url(https://steamcdn-a.akamaihd.net/steamcommunity/public/images/items/526920/7d3c2adfab2fa148b7b657e6af4a54413b71ab72.jpg););">
 	</body>
--- localizacao.php
+++ localizacao.php
@@ -2,9 +2,14 @@
 include("cabecalho_localizacao.php");
 include("navbar.php");
  ?>
+		 <br>
+ 			<center><h1 style="color: white"> Alô Cornos de plantão, vem da uma olhada </h1>
+ 				<br>
+ 	<iframe style="border:10px solid #000000" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d326.52383281692465!2d-49.95950096697044!3d-22.194676894545747!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94bfd6e87c8224f7%3A0x276cb165e42d9dac!2sDona%20Zinha%20Cafeteria!5e0!3m2!1spt-BR!2sbr!4v1566877228308!5m2!1spt-BR!2sbr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe></center>
  	<button onclick="topFunction()" id="myBtn" title="Go to top" class="btn btn-primary">Voltar ao topo</button>
 	<body style="background-image: url(https://steamcdn-a.akamaihd.net/steamcommunity/public/images/items/526920/7d3c2adfab2fa148b7b657e6af4a54413b71ab72.jpg););">
 	</body>
  <?php  
+ 
 include("rodape.php");
 ?>
